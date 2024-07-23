f3b8ab6c22f780e4995a99899c1c7755b653d833
Sushilkumar-Londhe <londhesushil11@gmail.com>
2024-07-23 09:50:30 +0530

    all section complelte

---
+++ javascript/JS loop/loop.js
@@ -0,0 +1,12 @@
+function printNumberPattern(n) {
+    for (let i = 1; i <= n; i++) {
+        let str = '';
+
+        for (let j = 1; j <= i; j++) {
+            str += j + ' ';}
+            
+        console.log(str.trim());
+    }
+}
+
+printNumberPattern(5);

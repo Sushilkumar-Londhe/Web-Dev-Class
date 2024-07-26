d36d6459160a940233bb5fc157c49d7a48252df4
Sushilkumar-Londhe <londhesushil11@gmail.com>
2024-07-26 12:04:43 +0530

    project complete

---
+++ javascript/square_hover/script.js
@@ -0,0 +1,25 @@
+const squareArray = document.getElementsByClassName("square");
+const colors = ["#e74c3c","#8e44ad","#3498db","#e67e22","2ecc71"];
+const defaultColor = "rgba(255,255,255,0.1)"
+
+for(const square of squareArray){
+    square.addEventListener("mouseenter",()=>{
+        const randomNum = parseInt(Math.random() * colors.length);
+
+        square.style.background = colors[randomNum];
+        
+        square.style.transition = "background-color 0s ease";
+
+        square.style.boxShadow = "0px 0px 0px 10px" + colors[randomNum];
+        });
+
+        square.addEventListener("mouseleave",()=>{
+
+        square.style.backgroundColor = defaultColor;
+
+        square.style.transition = "background-color 1s ease";
+
+        square.style.boxShadow = "";
+        
+    });
+}
